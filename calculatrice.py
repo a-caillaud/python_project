3a5daf4a2c4696d5c8c58ff067536add3d76b64b
Caillaud <caillaud@MacBook-Pro-de-Administrateur.local>
2019-10-25 23:07:50 +0200

    Changes to be committed: 	new file:   calculatrice.py 	new file:   test.py

---
+++ calculatrice.py
@@ -0,0 +1,3 @@
+a = int(input("Entrez un premier nombre : "))
+b = int(input("Entrez un second nombre : "))
+print(f"L'addition de {a} + {b} est égal à {a + b}")
---
+++ test.py
@@ -0,0 +1,6 @@
+# Variables choisies par l'utilisateur
+nombre_choisi1 = int(input("Entrez un premier nombre : "))
+nombre_choisi2 = int(input("Entrez un second nombre : "))
+
+# On se sert de la concaténation pour afficher la phrase contenant le résultat de l'addition
+print(f"Le résultat de l'addition de {nombre_choisi1} avec {nombre_choisi2} est égal à {nombre_choisi1 + nombre_choisi2}")
