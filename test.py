76bd2639e77f112ed963e6efc1dfe091bd8637c3
Caillaud <caillaud@MacBook-Pro-de-Administrateur.local>
2019-10-27 20:47:04 +0100

    v2

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
-a = "J'ai une classe de " + str(30) + "  élèves"
-b = str(10) + " + " + " 5" + " est égal à " + str(15)
+a = "J'ai une classe de " + str("30") + " élèves"
+b = str("10") + " + " + "5" + " est égal à " + str("15")
 c = 10 + int("5")
-d = "L'addition de 10 + 5 est égal à " + str(10 + 5)
+d = f"L'addition de 10 + 5 est égal à {10 + 5}"
 
 print(a)
 print(b)
